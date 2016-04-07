f97fc456c4d090ab02fef831fd2ce407977fd847
Jack Wohlfert <jack.wohlfert@gmail.com>
2016-04-06 23:51:20 -0400

    Change what is this text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,8 @@
     <div class="background" style="height: 700px; text-align: center;">
         <h1 style="font-size: 100px; margin-bottom: 60px; text-transform: uppercase">LDOC Breakfast</h1>
 
-        <p class="lead" style="margin-bottom: 60px; font-size: 24px">Start off your LDOC right with all you can
-            eat/drink breakfast at
-            Jack &amp; Andy's</p>
+        <p class="lead" style="margin-bottom: 60px; font-size: 24px">Start off your LDOC right with breakfast at Jack
+            &amp; Andy's</p>
 
         <div style="text-align: center">
             <a class="btn btn-primary btn-lg" href="#menu">See the Menu</a>
@@ -17,11 +16,17 @@
         <div class="container" style="text-align: center; width: 800px">
             <h2>What the hell is this?</h2>
 
-            <p>We love to cook. We love our friends. We love to give back. We thought, why not combine them and have an
-                LDOC breakfast where all the proceeds go to charity.</p>
+            <p>At 301 Henderson, we love to cook; we love our friends; and we love to give back. Last year, we decided
+                to have a breakfast on LDOC and invite our friends over to start the day off right. It was great time
+                all around. This year, we want to take it up a notch and do something good for the community in the
+                process. For $10, you can enjoy our homemade breakfast and
+                drink/eat as much as you want (see the menu below). All money that we make from the breakfast,
+                will be donated to TABLE NC.
+                So please join us on LDOC around 10AM and start your LDOC off right! </p>
         </div>
     </div>
-    <div class="background" style="height: 400px; text-align: center; background-image: url('/img/franklin-st.jpg')" id="menu">
+    <div class="background" style="height: 400px; text-align: center; background-image: url('/img/franklin-st.jpg')"
+         id="menu">
         <div class="title">
             <h1 style="border-color: white">Menu</h1>
         </div>
