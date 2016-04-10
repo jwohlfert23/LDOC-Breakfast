6a27ab1f2c4c6b7e7f8f3f74d5fa9c75e18b07bb
Jack Wohlfert <jack.wohlfert@gmail.com>
2016-04-10 16:19:34 -0400

    From name and address

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'no-reply@surge4.com', 'name' => "LDOC Breakfast"],
 
     /*
     |--------------------------------------------------------------------------
